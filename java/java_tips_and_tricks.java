27d71ad21241a5d09c415328d123878b617a3d7d
Shun Lin <s92014721@berkeley.edu>
2017-10-15 03:27:19 -0700

    added list and array

--- java/java_tips_and_tricks.java
+++ java/java_tips_and_tricks.java
@@ -10,3 +10,18 @@
 /**
  * this is a javaDoc comment, for writing comments for api
  */
+
+///// list /////
+
+// initialization
+
+List<Integer> list_of_intergers = new ArrayList<Integer>();
+
+
+
+///// array /////
+
+// initializatin
+
+int[] array_of_integers = int[size_of_array];
+// can not change size after initialization, if you want to change size, use list instead
